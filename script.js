0189b9318ec6b8f9ce0ece757c48c3f08cb3d7fc
DiegoP2001 <113427285+DiegoP2001@users.noreply.github.com>
2023-12-31 11:22:39 +0100

    Music Added

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var video = document.getElementById('background-video');
+
+    // Ajusta la velocidad de reproducción del video
+    video.playbackRate = 0.5; // Puedes ajustar este valor según tus necesidades
+});
