feeebfbe1dec7cfc5f157d3751c8e5106ea5b243
DiegoP2001 <113427285+DiegoP2001@users.noreply.github.com>
2023-12-31 11:30:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     var video = document.getElementById('background-video');
-
+    var audio = document.getElementById('miAudio');
     // Ajusta la velocidad de reproducción del video
-    video.playbackRate = 0.5; // Puedes ajustar este valor según tus necesidades
+    video.playbackRate = 0.6; // Puedes ajustar este valor según tus necesidades
+    audio.play();
 });
